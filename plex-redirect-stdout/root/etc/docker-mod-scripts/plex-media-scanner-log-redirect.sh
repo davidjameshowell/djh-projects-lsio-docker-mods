e4015e401fcff8ecf10b9edb2dcee49cbbbc6148
David Howell <david.howell@daxterfellowesservers.com>
2019-12-08 02:45:25 -0700

    Update scripts and remove services.

--- plex-redirect-stdout/root/etc/docker-mod-scripts/plex-media-scanner-log-redirect.sh
+++ plex-redirect-stdout/root/etc/docker-mod-scripts/plex-media-scanner-log-redirect.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/with-contenv bash
 
-bash -c 'tail -f  "/config/Library/Application Support/Plex Media Server/Logs/Plex Media Server.log" | grep --line-buffered .  > /proc/1/fd/1'
+tail -f  "/config/Library/Application Support/Plex Media Server/Logs/Plex Media Server.log" > /proc/1/fd/1
