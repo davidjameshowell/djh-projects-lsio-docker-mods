4fad0bd832de909cc1655510efd595d04278aaa5
davidjameshowell <david@davidjameshowell.com>
2020-10-22 08:53:30 +0000

    Update Rotate script, update Base tooling.

--- rclone-rotate-service-account/root/etc/services.d/sa-rotate/rotate.sh
+++ rclone-rotate-service-account/root/etc/services.d/sa-rotate/rotate.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-service_accounts=( ${SERVICE_ACCOUNTS_ROTATE_ACCOUNTS} )
-echo "Sleeping for ${SERVICE_ACCOUNT_ROTATE_INITIAL_SLEEP} before starting"
-sleep ${SERVICE_ACCOUNT_ROTATE_INITIAL_SLEEP}
+remote_list=$(rclone rc config/listremotes | jq --raw-output '.remotes[]')
+
 while :
     do
-    for i in "${service_accounts[@]}"
-    do
-        echo "Rotating service account to /service_account_keys/${i}"
-        # Unsure if we need to use the regular backend set or of it is redundant with RC backend
-        rclone --config=/rclone/rclone.conf backend set ${RCLONE_CONFIG_NAME}: -o service_account_file=/service_account_keys/${i}
-        rclone rc backend/command command=set fs=${RCLONE_CONFIG_NAME}: -o service_account_file=/service_account_keys/${i}
-
-        echo "Validating that we rotated properly."
-        rclone --config=/rclone/rclone.conf backend get ${RCLONE_CONFIG_NAME}: -o service_account_file
-        rclone rc backend/command command=get fs=${RCLONE_CONFIG_NAME}: -o service_account_file
+        for remote in $remote_list
+        do
+            type=$(rclone rc config/get name=$remote | jq --raw-output '.type')
+            echo $type
+            if [[ "$type" = "drive" ]]; then
+                echo "Modifying service accounts for - ${remote}"
+                SERVICE_ACCOUNT_KEY=$(curl ${SERVICE_ACCOUNT_BLACKLIST_ENDPOINT}/get/${DOCKER_SERVICE_NAME} | jq --raw-output '.service_account')
+                rclone rc backend/command command=set fs=${remote}: -o service_account_file=/service_account_keys/${SERVICE_ACCOUNT_KEY}
+                # Blacklist the key for one hour
+                key_to_blacklist=$(rclone rc backend/command command=get fs=${remote}: -o service_account_file | jq --raw-output '.result.service_account_file')
+            else
+                echo "Skipping as it is either a union or other non supported mount to rotate. Only manipulate Google Drive Mounts."
+            fi
+        done 
         sleep ${SERVICE_ACCOUNT_ROTATE_SLEEP}
     done
-done
-
-rclone rc backend/command command=get fs=${RCLONE_CONFIG_NAME}: -o service_account_file
